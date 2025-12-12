eb0bbef24aa1f5cfcb261c896ba16222671e0c20
vijay <vijayrajpallapu3210@gmail.com>
2025-12-12 13:18:02 +0530

    first

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  {languageOptions: { globals: globals.node }},
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "semi": ["error", "always"],
+      "quotes": ["error", "single"]
+    }
+  }
+];
